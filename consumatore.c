971bbe9d5ebfa48c34fb36bd359682bcd6aab762
fur1n0 <92094446+fur1n0@users.noreply.github.com>
2021-10-11 12:34:16 +0200

    Add files via upload
    version 2

--- consumatore.c
+++ consumatore.c
@@ -11,21 +11,24 @@ int main(int argc, char* argv[]){
 	int nread, fd,i ,j=0,k=0;
     char prefix[MAX_STRING_LENGTH];
 	//controllo numero argomenti
-	if (argc != 3 ){ 
+	if (argc != 3 && argc!=2){ 
 		perror(" numero di argomenti sbagliato"); exit(1);
 	} 
 	strcpy(prefix,argv[1]);
     //strcpy(prefix,argv[1]);
-    /*if(argc == 3){
+    if(argc == 3){
         
-    }*/
-	file_in = argv[2];
+        file_in = argv[2];
+        
+        fd = open(file_in, O_RDONLY);
+    
+        if (fd<0){
+            perror("P0: Impossibile aprire il file.");
+            exit(2);
+        }
+    }
+    else fd=0;
     printf("prefix = %s\n",prefix);
-	fd = open(file_in, O_RDONLY);
-	if (fd<0){
-		perror("P0: Impossibile aprire il file.");
-		exit(2);
-	}
 
 	while(nread = read(fd, &read_char, sizeof(char))) /* Fino ad EOF*/{
 		if(nread>=0){
@@ -38,24 +41,20 @@ int main(int argc, char* argv[]){
                         buf[k]=read_char;
                         k++;
                     }
-                    /*else{
-                        write(1, read_char, sizeof(char));
-                        printf("%c",read_char);
-                    }*/
                 }
                 else if(read_char!=prefix[k] && k!=0){
                     buf[k+1]='\0';
                     k=0;
-                    //write(1, buf, sizeof(char)*strlen(buf));
-                    printf("%s",buf);
+                    write(1, buf, sizeof(char)*strlen(buf));
+                    //printf("%s",buf);
                     
-                   // write(1, read_char, sizeof(char));
-                    printf("%c",read_char);
+                    write(1, &read_char, sizeof(char));
+                    //printf("%c",read_char);
                 }
                 else if(read_char!=prefix[k] && k==0){
                     k=0;
-                   // write(1, read_char, sizeof(char));
-                    printf("%c",read_char);
+                    write(1, &read_char, sizeof(char));
+                    //printf("%c",read_char);
                 }
         }
 		else{
